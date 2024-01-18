b6ed1f1754868abc86ba3b85246980512a7c7e81
David peralta <programador1@duxon.com.mx>
2024-01-18 09:51:52 -0600

    se sube footer y se cambian colores primarios

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+
+
+//*******************Metodos
+
+function abrirImagenLogo() {
+    let btnAbrirModal = document.getElementById("btnLogoNavbar");
+    let miModal = document.getElementById("modalLogoNavbar");
+
+    // Agrega un evento de clic al botón para abrir el modal
+    miModal.addEventListener('shown.bs.modal', () => {
+       
+      });
+}
+
+
+
