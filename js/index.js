8a25755e5f965c4763eb581f7485957b9b542c43
David peralta <programador1@duxon.com.mx>
2024-02-02 09:06:28 -0600

    lalalala

--- js/index.js
+++ js/index.js
@@ -4,14 +4,13 @@
 //*******************Metodos
 
 function abrirImagenLogo() {
-    let btnAbrirModal = document.getElementById("btnLogoNavbar");
-    let miModal = document.getElementById("modalLogoNavbar");
+  let btnAbrirModal = document.getElementById("btnLogoNavbar");
+  let miModal = document.getElementById("modalLogoNavbar");
 
-    // Agrega un evento de clic al botón para abrir el modal
-    miModal.addEventListener('shown.bs.modal', () => {
-       
-      });
+  // Agrega un evento de clic al botón para abrir el modal
+  miModal.addEventListener('shown.bs.modal', () => {
+
+  });
 }
-
 
 
